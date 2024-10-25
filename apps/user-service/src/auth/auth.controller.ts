2da6d3da92a8e8127c59cd006af4e29a35b7c408
AndreSuryana <Andresuryana17@gmail.com>
2024-10-25 11:14:02 +0700

    refactor: move `registerUser` from `UserController` to `AuthController`

--- apps/user-service/src/auth/auth.controller.ts
+++ apps/user-service/src/auth/auth.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import {
+    RegisterUserRequest,
+    RegisterUserResponse,
+} from '@grp/proto/user/user-service';
+import { validateConvertDto } from 'src/common/helpers/validation-pipe.helper';
+import { handleError } from 'src/common/handlers/exception.handler';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    private readonly logger: Logger;
+
+    constructor(private readonly userService: UserService) {
+        this.logger = new Logger(AuthController.name, {
+            timestamp: true,
+        });
+    }
+
+    @GrpcMethod('UserService', 'RegisterUser')
+    async registerUser(
+        request: RegisterUserRequest,
+    ): Promise<RegisterUserResponse> {
+        try {
+            const createUserDto = await validateConvertDto(CreateUserDto, request);
+
+            const user = await this.userService.createUser(createUserDto);
+            this.logger.log(`New user registered successfully: ${user.id}`);
+
+            return {
+                userId: user.id,
+                message: 'User registered successfully',
+            };
+        } catch (e) {
+            this.logger.error(`Error registering user: ${e.message}`, e.stack);
+            throw handleError(e);
+        }
+    }
+}
--- apps/user-service/src/user/user.controller.ts
+++ apps/user-service/src/user/user.controller.ts
@@ -1,39 +1,4 @@
 import { Controller, Logger } from '@nestjs/common';
-import { UserService } from './user.service';
-import { GrpcMethod } from '@nestjs/microservices';
-import {
-  RegisterUserRequest,
-  RegisterUserResponse,
-} from '@grp/proto/user/user-service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { validateConvertDto } from 'src/common/helpers/validation-pipe.helper';
-import { handleError } from 'src/common/handlers/exception.handler';
 
 @Controller()
-export class UserController {
-  constructor(private readonly userService: UserService) {}
-
-  private readonly logger = new Logger(UserController.name, {
-    timestamp: true,
-  });
-
-  @GrpcMethod('UserService', 'RegisterUser')
-  async registerUser(
-    request: RegisterUserRequest,
-  ): Promise<RegisterUserResponse> {
-    try {
-      const createUserDto = await validateConvertDto(CreateUserDto, request);
-
-      const user = await this.userService.createUser(createUserDto);
-      this.logger.log(`New user registered successfully: ${user.id}`);
-
-      return {
-        userId: user.id,
-        message: 'User registered successfully',
-      };
-    } catch (e) {
-      this.logger.error(`Error registering user: ${e.message}`, e.stack);
-      throw handleError(e);
-    }
-  }
-}
+export class UserController {}
