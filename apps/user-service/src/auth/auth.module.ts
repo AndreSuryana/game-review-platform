5d292033abdc1b9b5bf4aeacf626bed6fdd9f04e
AndreSuryana <Andresuryana17@gmail.com>
2024-11-21 17:04:40 +0700

    fix(auth): import `UserModule` rather than `UserServie` as providers

--- apps/user-service/src/auth/auth.module.ts
+++ apps/user-service/src/auth/auth.module.ts
@@ -1,25 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UserService } from 'src/user/user.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/user/schemas/user.schema';
 import { SessionService } from 'src/session/session.service';
 import { SessionModule } from 'src/session/session.module';
 import { EmailModule } from 'src/email/email.module';
 import { RedisModule } from 'src/redis/redis.module';
 import { TokenModule } from 'src/token/token.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     SessionModule,
     RedisModule,
     EmailModule,
     TokenModule,
+    UserModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, UserService, SessionService],
+  providers: [AuthService, SessionService],
   exports: [AuthService],
 })
 export class AuthModule {}
